a399ea1227822a8e845d722b3b1aec8a7a236582
compgal01 <118755558+compgal01@users.noreply.github.com>
2025-10-10 18:08:13 -0700

    Sprint 2: Springboot and database setup with integrated html mockup

---
+++ code/backend/src/test/java/com/voilandPantry/BackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.voilandPantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BackendApplication.class)
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
